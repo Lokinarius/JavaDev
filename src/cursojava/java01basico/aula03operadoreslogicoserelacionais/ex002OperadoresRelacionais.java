f21f3d300971797e5d428f8c16b8de1b8dfebd45
William Galvão <Liam_draven@outlook.com>
2025-11-16 16:08:53 -0300

    Delete ex002OperadoresRelacionais.java

--- src/cursojava/java01basico/aula03operadoreslogicoserelacionais/ex002OperadoresRelacionais.java
+++
@@ -1,8 +0,0 @@
-package cursojava.java01basico.aula03operadoreslogicoserelacionais;
-
-public class ex002OperadoresRelacionais {
-    public static void main(String[] args) {
-        System.out.println("Operadores Relacionais");
-
-    }
-}
